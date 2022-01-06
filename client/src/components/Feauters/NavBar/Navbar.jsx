0f34eb579d356a531d0cbac9710623655145c228
eliasadmaso1 <eliasadmas01@gmail.com>
2022-01-06 20:54:51 +0200

    fix-icons

--- client/src/components/Feauters/NavBar/Navbar.jsx
+++ client/src/components/Feauters/NavBar/Navbar.jsx
@@ -6,7 +6,7 @@ import {
   Badge,
   Typography,
 } from "@material-ui/core";
-import './Navbar.css';
+import "./Navbar.css";
 import { Link } from "react-router-dom";
 import Nike from "../../../images/logos/nike.png";
 import { getCartProducts } from "../../../Service/cart-service";
@@ -20,13 +20,15 @@ import {
 } from "@ant-design/icons";
 
 export default function Navbar() {
-  const {  user } = useMyContext();
+  const { user } = useMyContext();
   const [products, setProducts] = useState([]);
   const userName = user?.firstName || "Member";
 
   const logOut = () => {
     localStorage.removeItem("token");
-    window.location.replace(process.env.NODE_ENV === 'production' ? "/shoes-store-app/":  "/");
+    window.location.replace(
+      process.env.NODE_ENV === "production" ? "/shoes-store-app/" : "/"
+    );
   };
 
   useEffect(() => {
@@ -39,90 +41,75 @@ export default function Navbar() {
   }, [user]);
 
   return (
-    <AppBar position="fixed" className="appBar" color="inherit" >
+    <AppBar position="fixed" className="appBar" color="inherit">
       <Toolbar>
         <img src={Nike} width="40px" />
-       
-       <Typography variant="h7" className="title" color="inherit">
-          <Link to="/"  className="link">
+
+        <Typography variant="h7" className="title" color="inherit">
+          <Link to="/" className="link">
             {" "}
             Home
           </Link>
-          <Link
-            to="/Accessories"
-            className="link"
-          >
+          <Link to="/Accessories" className="link">
             Accessories
           </Link>
-          <Link to="/Contact"  className="link">
+          <Link to="/Contact" className="link">
             Contact
           </Link>
 
-          <Link to="/About"  className="link">
+          <Link to="/About" className="link">
             About
           </Link>
-          
         </Typography>
-        
 
-        <div >
-      <IconButton>
-            <Link
-              to="/Login"
-              style={{ color: "black", textDecoration: "none",fontWeight:"bold",fontSize:"18px" }}
-            >
-              
-              {user ? `Hello, ${userName}` : null}
-              <UserOutlined
-                style={{
-                  fontSize: "22px",
-                  marginLeft: "15px",
-                  marginTop: "6px",
-                }}
-              />
-            </Link>
-          </IconButton>
-         
-
-          {user ? (
-            <IconButton aria-label="show cart items" color="inherit">
-              <UserDeleteOutlined
-                onClick={logOut}
-                style={{marginTop: "2px", fontSize: "24px"}}
-              />{" "}
-            </IconButton>
-          ) : null}
-
-          {user ? (
+        <div>
+          {user ? null : (
             <IconButton>
-              <Link to="/Favorites">
-                <HeartOutlined
+              <Link
+                to="/Login"
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                  fontWeight: "bold",
+                  fontSize: "18px",
+                }}
+              >
+                {" "}
+                <UserOutlined
                   style={{
-                    fontSize: "23px",
-                    marginTop: "-35px",
-                    marginLeft: "5px",
+                    fontSize: "22px",
+                    marginLeft: "15px",
+                    marginTop: "6px",
                   }}
                 />
               </Link>
             </IconButton>
-          ) : null}
+          )}
 
           {user ? (
-            <IconButton>
-              <Badge
-                badgeContent={user ? products.length : null}
-                color="secondary"
-              >
-                <Link
-                  to="/CartPage"
-                  style={{ color: "black", textDecoration: "none" }}
-                >
-                  <ShoppingCartOutlined
-                    style={{ fontSize: "26px", marginTop: "4px" }}
-                  />
+            <div className="icons">
+              <IconButton aria-label="show cart items" color="inherit">
+                <UserDeleteOutlined onClick={logOut} className="link1" />{" "}
+              </IconButton>
+              <IconButton>
+                <Link to="/Favorites"
+                className="link2">
+                  <HeartOutlined />
                 </Link>
-              </Badge>
-            </IconButton>
+              </IconButton>
+              <IconButton>
+                <Badge
+                  badgeContent={user ? products.length : null}
+                  color="secondary"
+                >
+                  <Link
+                    to="/CartPage"
+                    className="link3"                  >
+                    <ShoppingCartOutlined />
+                  </Link>
+                </Badge>
+              </IconButton>
+            </div>
           ) : null}
         </div>
       </Toolbar>
