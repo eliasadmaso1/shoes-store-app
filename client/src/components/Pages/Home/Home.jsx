6f340906316eb4ce31260f5a1f16250461b07f39
eliasadmaso1 <eliasadmas01@gmail.com>
2022-01-09 16:51:43 +0200

    accessories-query

--- client/src/components/Pages/Home/Home.jsx
+++ client/src/components/Pages/Home/Home.jsx
@@ -41,14 +41,14 @@ export default function Home() {
         <div className="home-images">
           <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f2279bda-f7d2-4687-aca9-c796c601e4f1/blazer-mid-77-jumbo-shoes-g1NlvJ.png" width="550px"/>
           <img src="https://static.nike.com/a/images/w_960,c_limit/56bd12a4-776d-48ba-9dab-a25003f16699/image.webp" width="550px" style={{position:"relative"}}/>
-          <button className="shoes-btn">Shop</button>
+         <button className="shoes-btn"> <Link to="/MenShoes" style={{color:"white"}}>Shop</Link></button>
           <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/ab6e3782-7067-4a7a-9b66-e0f80aff1c60/air-force-1-low-07-shoes-kGqrWx.png" width="550px"/>
 
 
         </div>
         <div className="accessories-div">
           <img src="https://static.nike.com/a/images/w_960,c_limit/1cab4279-0152-4044-8851-233784df0c3c/image.webp" style={{position:"relative"}}/>
-          <button className="acc-btn">Accessories</button>
+         <button className="acc-btn"> <Link to="/Accessories" style={{color:"white"}}>Accessories</Link></button>
         </div>
 
         <Footer />
