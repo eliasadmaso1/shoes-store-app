302fa3bb4dd030bbcb9230daf919073bc47b56f3
eliasadmaso1 <eliasadmas01@gmail.com>
2022-04-12 18:56:40 +0300

    footer-update

--- client/src/components/Feauters/Footer/Footer.jsx
+++ client/src/components/Feauters/Footer/Footer.jsx
@@ -1,17 +1,54 @@
-import React from 'react';
-import './footer.css';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import {Link } from 'react-router-dom';
+import React from "react";
+import "./footer.css";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
-export default function Footer(){
-    return(<><div className="footerDiv">
-
-        <a href="https://www.facebook.com/elias.admaso"><FacebookIcon style={{color:"gray",fontSize:"40px",padding:"5px",marginRight:"5px"}}></FacebookIcon></a>
-        <a href="https://github.com/eliasadmaso1"><GitHubIcon style={{color:"gray",fontSize:"40px",padding:"5px",marginRight:"5px"}}></GitHubIcon></a>
-        <a href="https://www.linkedin.com/in/elias-admaso-152790206/"><LinkedInIcon style={{color:"gray",fontSize:"40px",padding:"5px",marginRight:"5px"}}></LinkedInIcon></a>
+export default function Footer() {
+  return (
+    <>
+    <div className="footer-container">
+    <div className="contact-us">
+        <h3>Contact-Us</h3>
+        <a href="https://www.facebook.com/elias.admaso">
+          <FacebookIcon
+            style={{
+              color: "gray",
+              fontSize: "25px",
+            }}
+          ></FacebookIcon>
+        </a>
+        <a href="https://github.com/eliasadmaso1">
+          <GitHubIcon
+            style={{
+              color: "gray",
+              fontSize: "25px",
+            }}
+          ></GitHubIcon>
+        </a>
+        <a href="https://www.linkedin.com/in/elias-admaso-152790206/">
+          <LinkedInIcon
+            style={{
+              color: "gray",
+              fontSize: "25px",
+            }}
+          ></LinkedInIcon>
+        </a>
+      </div>
+      <div className="services">
+          <h3>Services</h3>
+          <h4>Orders</h4>
+          <h4>User convenience</h4>
+          <h4>Best Products</h4>
+      </div>
+      <div className="coming-soon">
+          <h3>Coming soon...</h3>
+          <h4>Nba Jerseys</h4>
+          <h4>Air Jordan Collections</h4>
+          <h4>Nike Track Suits</h4>
+      </div>
     </div>
-    </>)
+      
+    </>
+  );
 }
